49ff1c81c20c2df1d065cdfffe368a9b39a6ba9e
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-13 23:20:10 -0500

    code Along

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchAdvice from "./componets/SearchAdvice";
+
+const App = () => {
+  return (
+    <div>
+      <SearchAdvice />
+    </div>
+  );
+};
+
+export default App;
